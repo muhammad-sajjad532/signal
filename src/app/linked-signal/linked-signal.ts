1c2da7f9fcf6886cd7a1246f15b1eccec7491915
Muhammad Sajjad <muhammadsajjad.0532@gmail.com>
2025-11-25 14:45:36 +0500

    created linked-signal component

---
+++ src/app/linked-signal/linked-signal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-linked-signal',
+  imports: [],
+  templateUrl: './linked-signal.html',
+  styleUrl: './linked-signal.scss',
+})
+export class LinkedSignal {
+
+}
